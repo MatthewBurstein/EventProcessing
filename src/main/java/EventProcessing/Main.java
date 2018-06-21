7b2b2134b30caa56f619791ae4e27b5817b79cc8
Julian Ng <julian.ng@softwire.com>
2018-06-21 17:30:58 +0100

    Started implementing logback framework - not yet working

--- src/main/java/EventProcessing/Main.java
+++ src/main/java/EventProcessing/Main.java
@@ -1,14 +1,18 @@
 package EventProcessing;
 
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Main {
-
+    static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws IOException {
+
         S3ObjectInputStream s3is = new S3Client().generateS3InputStream();
         DataReader reader = new DataReader();
+        logger.debug("AWS S3 connection established");
         reader.saveToFile(s3is);
     }
 
--- src/main/java/EventProcessing/S3Client.java
+++ src/main/java/EventProcessing/S3Client.java
@@ -1,17 +1,26 @@
 package EventProcessing;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class S3Client {
+    Logger logger = LoggerFactory.getLogger("S3Client");
+
     public AmazonS3 clientBuilder() {
+
         final AmazonS3 s3 = AmazonS3ClientBuilder
                 .standard()
                 .withRegion(System.getenv("AWS_REGION"))
                 .build();
+
+//        logger.debug("AWS S3 connection established");
 
         return s3;
     }
