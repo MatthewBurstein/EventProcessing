3c487016efc83eb8e70cbbf14a47df07d99a113a
Julian Ng <julian.ng@softwire.com>
2018-06-26 16:59:31 +0100

    Created TimeManager - started writing test

---
+++ src/test/java/TimeManagerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class TimeManagerTest {
+    @Test
+    public void createTimeRange_createsASingleTimeRange() {
+
+    }
+
+    @Test
+    public void timeManagerConstructor_createsFiveTimeRanges() {
+
+    }
+}
