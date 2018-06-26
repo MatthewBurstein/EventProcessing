f7d159d99aee42115c37d88b4565e54a27b59dbd
Matt Burstein <matthew.burstein@softwire.com>
2018-06-26 13:57:18 +0100

    feat: implement Analyser#getAvergaeValue

--- src/main/java/eventprocessing/analysis/Analyser.java
+++ src/main/java/eventprocessing/analysis/Analyser.java
@@ -1,7 +1,19 @@
 package eventprocessing.analysis;
 
+import eventprocessing.models.Response;
+import eventprocessing.models.ResponseList;
+
 public class Analyser {
 
+    private static double reduceValues(double currentValue, double accumulator) {
+        return accumulator + currentValue;
+    }
 
+    public double getAverageValue(ResponseList responseList) {
+        double total = responseList.getResponses().stream()
+                .map(response -> response.getMessage().getValue())
+                .reduce(Double.valueOf(0), Analyser::reduceValues);
+        return total / responseList.getResponses().size();
+    }
 
 }
--- src/main/java/eventprocessing/models/Message.java
+++ src/main/java/eventprocessing/models/Message.java
@@ -3,11 +3,14 @@ package eventprocessing.models;
 public class Message {
     private String locationId;
     private String eventId;
-    private String value;
+    private double value;
     private String timestamp;
 
     public String getLocationId() {
         return locationId;
     }
 
+    public double getValue() {
+        return value;
+    }
 }
