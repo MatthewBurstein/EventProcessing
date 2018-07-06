feb3608196d2751cb244a154a17efc71e4d273f8
Julian Ng <julian.ng@softwire.com>
2018-07-06 12:16:16 +0100

    refactor: BucketTest - extract mock creation to private methods

--- src/test/java/BucketTest.java
+++ src/test/java/BucketTest.java
@@ -9,6 +9,7 @@ import org.threeten.extra.MutableClock;
 
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -60,40 +61,61 @@ public class BucketTest {
 
     @Test
     public void getReadingIds_returnsListOfReadingIds() {
-        Reading reading1 = Mockito.mock(Reading.class);
-        when(reading1.getId()).thenReturn("reading1Id");
-        Reading reading2 = Mockito.mock(Reading.class);
-        when(reading2.getId()).thenReturn("reading2Id");
-        bucket.getReadings().addAll(Lists.newArrayList(reading1, reading2));
+        List<Reading> readings = buildMultipleReadingsWithIds("reading1Id", "reading2Id");
+        bucket.getReadings().addAll(readings);
         List<String> expected = Lists.newArrayList("reading1Id", "reading2Id");
         assertEquals(bucket.getReadingIds(), expected);
     }
 
     @Test
     public void getAverageValue_returnsAverageOfReadingValues() {
-        Reading reading = Mockito.mock(Reading.class);
-        List<Reading> readings = Lists.newArrayList(reading, reading, reading);
+        List<Reading> readings = buildMultipleReadingsWithValues(1.0, 2.0, 3.0);
         bucket.getReadings().addAll(readings);
-        when(reading.getValue()).thenReturn(1.0, 2.0, 3.0);
-
         double expectedAverageValue = 2.0;
         assertEquals(expectedAverageValue, bucket.getAverageValue(), 0);
     }
 
     @Test
     public void isDuplicateReading_whenDuplicate_returnsTrue() {
-        Reading reading = Mockito.mock(Reading.class);
-        when(reading.getId()).thenReturn("readingId");
+        Reading reading = buildReadingWithId("readingId");
         bucket.addResponse(reading);
         assertTrue(bucket.isDuplicateReading(reading));
     }
 
     @Test
     public void isDuplicateReading_whenNotDuplicate_returnsFalse() {
-        Reading reading = Mockito.mock(Reading.class);
-        when(reading.getId()).thenReturn("readingId");
+        Reading reading = buildReadingWithId("readingId");
         assertFalse(bucket.isDuplicateReading(reading));
     }
+
+    private Reading buildReadingWithId(String readingId) {
+        Reading reading = Mockito.mock(Reading.class);
+        when(reading.getId()).thenReturn(readingId);
+        return reading;
+    }
+
+    private List<Reading> buildMultipleReadingsWithIds(String ... readingIds) {
+        List<Reading> readings = new ArrayList<>();
+        for(String readingId : readingIds) {
+            readings.add(buildReadingWithId(readingId));
+        }
+        return readings;
+    }
+
+    private Reading buildReadingWithValue(double value) {
+        Reading reading = Mockito.mock(Reading.class);
+        when(reading.getValue()).thenReturn(value);
+        return reading;
+    }
+
+    private List<Reading> buildMultipleReadingsWithValues(double ... values) {
+        List<Reading> readings = new ArrayList<>();
+        for(double value : values) {
+            readings.add(buildReadingWithValue(value));
+        }
+        return readings;
+    }
+
 
     private MutableClock buildClock(long millis) {
         Instant fixedInstant = Instant.ofEpochMilli(millis);
