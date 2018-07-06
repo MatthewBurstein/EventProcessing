7622d3afed0fb62d3c69f8f31d5197c486c37cbb
Matt Burstein <matthew.burstein@softwire.com>
2018-07-06 16:29:22 +0100

    feat: implement ResponseProcessorThread

--- src/main/java/eventprocessing/Main.java
+++ src/main/java/eventprocessing/Main.java
@@ -1,17 +1,18 @@
 package eventprocessing;
 
-import com.amazonaws.services.sqs.model.*;
-import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.google.common.collect.Lists;
 import eventprocessing.amazonservices.AmazonController;
 import eventprocessing.amazonservices.SqsClient;
-import eventprocessing.customerrors.InvalidSqsResponseException;
 import eventprocessing.fileservices.CSVFileService;
 import eventprocessing.fileservices.JSONParser;
-import eventprocessing.models.*;
+import eventprocessing.models.ReadingAggregator;
+import eventprocessing.models.SensorList;
+import eventprocessing.models.TemporarySqsResponseStorage;
 import eventprocessing.responseservices.SqsResponseService;
+import eventprocessing.threads.ResponseProcessorThread;
 import eventprocessing.threads.SqsClientThread;
-import org.apache.commons.lang3.time.StopWatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,7 +28,6 @@ public class Main {
     private static SqsResponseService sqsResponseService;
     private static Scanner scanner;
     private static AmazonController amazonController;
-    private static StopWatch stopWatch;
     private static CSVFileService csvFileService;
     private static ReadingAggregator readingAggregator;
     private static TemporarySqsResponseStorage temporarySqsResponseStorage;
@@ -45,63 +45,44 @@ public class Main {
         ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
         System.out.println("How many minutes to run for?\nMUST be at least 1 minute longer than " + gc.MAX_MESSAGE_DELAY_MINS + " mins");
         int duration = scanner.nextInt();
-        int notWorkingSensorCount = 0;
-        stopWatch.start();
-
-        int messageCounter = 0;
 
 
-        long tensOfSeconds = 0;
+        SqsClientThread sqsClientThread1 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest, queueUrl);
+        SqsClientThread sqsClientThread2 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest, queueUrl);
+        SqsClientThread sqsClientThread3 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest, queueUrl);
+        ResponseProcessorThread responseProcessorThread = createAndStartResponseProcessorThread(sensorList);
 
-        SqsClientThread sqsClientThread1 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest);
-        SqsClientThread sqsClientThread2 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest);
-        SqsClientThread sqsClientThread3 = createAndStartSqsClientThread(sqsClient, receiveMessageRequest);
-        while (stopWatch.getTime() < (duration*60000)) {
+        runForMinutes(duration, sqsClient, queueUrl);
 
-//            KEEP FOR LOGGING QUEUE SIZE
-            if (tensOfSeconds == stopWatch.getTime() / 10000) {
+        sqsClientThread1.terminate();
+        sqsClientThread2.terminate();
+        sqsClientThread3.terminate();
+        responseProcessorThread.terminate();
+    }
+
+    private static ResponseProcessorThread createAndStartResponseProcessorThread(SensorList sensorList) {
+        ResponseProcessorThread responseProcessorThread = new ResponseProcessorThread(temporarySqsResponseStorage, sensorList, readingAggregator);
+        responseProcessorThread.start();
+        return responseProcessorThread;
+    }
+
+    private static void runForMinutes(int duration, SqsClient sqsClient, String queueUrl) {
+        long tensOfSeconds = System.currentTimeMillis() / 10000;
+        long endTime = System.currentTimeMillis() + duration * 60000;
+        while (System.currentTimeMillis() < endTime) {
+//      Uncomment to log queue size
+            if (tensOfSeconds == System.currentTimeMillis() / 10000) {
                 GetQueueAttributesRequest attr = new GetQueueAttributesRequest(queueUrl);
                 attr.setAttributeNames(Lists.newArrayList("ApproximateNumberOfMessages"));
                 Map<String, String> attributesMap = sqsClient.getSqs().getQueueAttributes(attr).getAttributes();
                 logger.info("queue size = " + attributesMap.get("ApproximateNumberOfMessages"));
                 tensOfSeconds++;
             }
-            ReceiveMessageResult messageResult = temporarySqsResponseStorage.take();
-
-            messageCounter += messageResult.getMessages().size();
-
-            for (Message msg : messageResult.getMessages()) {
-                try {
-                    SqsResponse sqsResponse = sqsResponseService.parseResponse(msg.getBody());
-                    Reading reading = new Reading(sqsResponse);
-                    if(sensorList.isWorkingSensor(reading)) {
-                        readingAggregator.process(reading);
-                    } else {
-                        notWorkingSensorCount++;
-                    }
-                } catch (InvalidSqsResponseException e) {
-                    logger.error("Invalid JSON string received" + e.getMessage());
-                    logger.error("Received json string: " + msg.getBody());
-                }
-
-            final String messageReceiptHandle = messageResult.getMessages().get(0).getReceiptHandle();
-            sqsClient.getSqs().deleteMessage(new DeleteMessageRequest(queueUrl, messageReceiptHandle));
-            }
-
-
         }
-        sqsClientThread1.terminate();
-        sqsClientThread2.terminate();
-        sqsClientThread3.terminate();
-        readingAggregator.processAllBuckets();
-        logger.info("Total messages received: " + messageCounter);
-        logger.info("Total duplicates in this run: " + readingAggregator.getDuplicateCounter());
-        logger.info("Total faulty sensors in this run: " + notWorkingSensorCount);
-
     }
 
-    private static SqsClientThread createAndStartSqsClientThread(SqsClient sqsClient, ReceiveMessageRequest receiveMessageRequest) {
-        SqsClientThread sqsClientThread = new SqsClientThread(temporarySqsResponseStorage, sqsClient, receiveMessageRequest);
+    private static SqsClientThread createAndStartSqsClientThread(SqsClient sqsClient, ReceiveMessageRequest receiveMessageRequest, String queueUrl) {
+        SqsClientThread sqsClientThread = new SqsClientThread(temporarySqsResponseStorage, sqsClient, receiveMessageRequest, queueUrl);
         sqsClientThread.start();
         return sqsClientThread;
     }
@@ -111,7 +92,6 @@ public class Main {
         sqsResponseService = new SqsResponseService();
         scanner = new Scanner(System.in);
         amazonController = new AmazonController();
-        stopWatch = new StopWatch();
         csvFileService = new CSVFileService("ResponseData" + System.currentTimeMillis() + ".csv");
         readingAggregator = new ReadingAggregator(csvFileService, Clock.systemUTC(), gc);
         temporarySqsResponseStorage = new TemporarySqsResponseStorage();
